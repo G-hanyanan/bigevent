13ced40f30c8e638329dd6032c10d02197370839
G-hanyanan <1209848443@qq.com>
2020-01-16 19:17:41 +0800

    优化ajax

--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -1,37 +1,16 @@
 var user = {
     login: function (name, password) {
-        $.post(APILIST.user_login, {
+        return $.post(APILIST.user_login, {
             'user_name': name,
             'password': password
-        }).then(function (res) {
-            if (res.code === 200) {
-                //    alert('登录成功') 
-                window.location.href = './index.html'
-            } else {
-                alert(res.msg)
-            }
         })
     },
     logout: function () {
-        $.post(APILIST.user_logout)
-            .then(function (res) {
-                if (res.code === 200) {
-                    window.location.href = './login.html'
-                } else {
-                    alert(res.msg)
-                }
-            })
+        return $.post(APILIST.user_logout)
+
     },
     logInfo: function () {
-        $.get(APILIST.user_Info)
-            .then(function (res) {
-                if (res.code === 200) {
-                    $("#userName").text(res.data.nickname);
-                    $(".user_pic").attr('src', res.data.user_pic)
-                } else {
-                    alert(res.msg)
-                }
+        return $.get(APILIST.user_Info)
 
-            })
     }
 }
