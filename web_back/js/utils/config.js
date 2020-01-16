a51a0f93ea5646c251ac259922cf609219ba9f09
G-hanyanan <1209848443@qq.com>
2020-01-16 18:13:31 +0800

    创建接口配置JS

---
+++ web_back/js/utils/config.js
@@ -0,0 +1,11 @@
+// 所有接口的基地址
+
+var BASEURL = 'http://192.168.172.82:8000'
+
+// 所有子接口
+
+var APILIST = {
+    user_login: BASEURL + '/admin/login',
+    user_logout: BASEURL + '/admin/logout',
+    user_Info: BASEURL + '/admin/getuser',
+}
